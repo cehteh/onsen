3f66c2d2b730bd71cda75cfb1533059470f2357b
Christian Thäter <ct@pipapo.org>
2022-07-29 14:43:29 +0200

    boilerplate PartialEq Eq PartialOrd Ord Hash Hasher

--- src/boxed.rs
+++ src/boxed.rs
@@ -1,6 +1,9 @@
 //! onsen::Box is WIP! many trait implementations of the std::boxed::Box are still missing.
 use std::borrow::Borrow;
 use std::borrow::BorrowMut;
+use std::cmp::Ordering;
+use std::hash::Hash;
+use std::hash::Hasher;
 use std::ops::Deref;
 use std::ops::DerefMut;
 
@@ -98,6 +101,105 @@ impl<T, const E: usize> AsMut<T> for Box<'_, T, E> {
     fn as_mut(&mut self) -> &mut T {
         self.slot.get_mut()
     }
+}
+
+impl<T: PartialEq, const E: usize> PartialEq for Box<'_, T, E> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        PartialEq::eq(&**self, &**other)
+    }
+    #[inline]
+    fn ne(&self, other: &Self) -> bool {
+        PartialEq::ne(&**self, &**other)
+    }
+}
+
+impl<T: PartialOrd, const E: usize> PartialOrd for Box<'_, T, E> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        PartialOrd::partial_cmp(&**self, &**other)
+    }
+    #[inline]
+    fn lt(&self, other: &Self) -> bool {
+        PartialOrd::lt(&**self, &**other)
+    }
+    #[inline]
+    fn le(&self, other: &Self) -> bool {
+        PartialOrd::le(&**self, &**other)
+    }
+    #[inline]
+    fn ge(&self, other: &Self) -> bool {
+        PartialOrd::ge(&**self, &**other)
+    }
+    #[inline]
+    fn gt(&self, other: &Self) -> bool {
+        PartialOrd::gt(&**self, &**other)
+    }
+}
+
+impl<T: Ord, const E: usize> Ord for Box<'_, T, E> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        Ord::cmp(&**self, &**other)
+    }
+}
+impl<T: Eq, const E: usize> Eq for Box<'_, T, E> {}
+
+impl<T: Hash, const E: usize> Hash for Box<'_, T, E> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        (**self).hash(state);
+    }
+}
+
+impl<T: Hasher, const E: usize> Hasher for Box<'_, T, E> {
+    fn finish(&self) -> u64 {
+        (**self).finish()
+    }
+    fn write(&mut self, bytes: &[u8]) {
+        (**self).write(bytes)
+    }
+    fn write_u8(&mut self, i: u8) {
+        (**self).write_u8(i)
+    }
+    fn write_u16(&mut self, i: u16) {
+        (**self).write_u16(i)
+    }
+    fn write_u32(&mut self, i: u32) {
+        (**self).write_u32(i)
+    }
+    fn write_u64(&mut self, i: u64) {
+        (**self).write_u64(i)
+    }
+    fn write_u128(&mut self, i: u128) {
+        (**self).write_u128(i)
+    }
+    fn write_usize(&mut self, i: usize) {
+        (**self).write_usize(i)
+    }
+    fn write_i8(&mut self, i: i8) {
+        (**self).write_i8(i)
+    }
+    fn write_i16(&mut self, i: i16) {
+        (**self).write_i16(i)
+    }
+    fn write_i32(&mut self, i: i32) {
+        (**self).write_i32(i)
+    }
+    fn write_i64(&mut self, i: i64) {
+        (**self).write_i64(i)
+    }
+    fn write_i128(&mut self, i: i128) {
+        (**self).write_i128(i)
+    }
+    fn write_isize(&mut self, i: isize) {
+        (**self).write_isize(i)
+    }
+    // fn write_length_prefix(&mut self, len: usize) {
+    //     (**self).write_length_prefix(len)
+    // }
+    // fn write_str(&mut self, s: &str) {
+    //     (**self).write_str(s)
+    // }
 }
 
 #[cfg(test)]
