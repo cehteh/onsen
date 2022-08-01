7d033fbbc9f53fc1a8c0469a0d164ef764c4cc51
Christian Thäter <ct@pipapo.org>
2022-08-01 21:03:23 +0200

    cleanup baseline benchmark

--- benches/baseline.rs
+++ benches/baseline.rs
@@ -3,6 +3,7 @@ use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criteri
 
 use onsen;
 
+// just simple allocate and instantly drop test
 fn rust_box_drop() {
     black_box(Box::new(0u64));
 }
@@ -11,24 +12,25 @@ fn onsen_box_drop<const E: usize>(pool: &onsen::Pool<u64, E>) {
     black_box(pool.alloc_box(0u64));
 }
 
+// allocate many elements into a preallocated Vec and drop them all at the end
 fn rust_box_many(howmany: usize) {
     let mut keep = Vec::with_capacity(howmany);
     for _ in 0..howmany {
-        keep.push(black_box(Box::new(0u64)));
+        keep.push(Box::new(0u64));
     }
 }
 
 fn onsen_box_many<'a, const E: usize>(howmany: usize, pool: &'a onsen::Pool<u64, E>) {
     let mut keep = Vec::with_capacity(howmany);
     for _ in 0..howmany {
-        keep.push(black_box(pool.alloc_box(0u64)));
+        keep.push(pool.alloc_box(0u64));
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut baseline = c.benchmark_group("baseline drop");
 
-    baseline.bench_function("rust box drop", { |b| b.iter(|| rust_box_drop()) });
+    baseline.bench_function("rust box drop", |b| b.iter(|| rust_box_drop()));
 
     baseline.bench_function("onsen box drop", {
         let pool = onsen::pool!(u64, PAGE);
@@ -36,7 +38,6 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 
     drop(baseline);
-
     let mut baseline = c.benchmark_group("baseline keep");
 
     for size in [1000usize, 5000usize, 10000usize, 50000usize, 100000usize].iter() {
