77a1c1cfda3b2752ec35c457be7d3084ca59f504
Christian Thäter <ct@pipapo.org>
2023-02-10 19:47:48 +0100

    reinstantiate arcpool

--- src/arcpool.rs
+++ src/arcpool.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use erasable::*;
 use std::sync::Arc;
 
 #[cfg(not(feature = "parking_lot"))]
@@ -7,11 +8,11 @@ use std::sync::Mutex;
 #[cfg(feature = "parking_lot")]
 use parking_lot::Mutex;
 
-/// A multithreaded, interior mutable memory Pool backed by a reference count.  This allows
+/// A single thread, interior mutable memory Pool backed by a reference count.  This allows
 /// objects to hold references back to the pool to keep it alive without carrying a lifetime.
-pub struct ArcPool<T: Sized>(Arc<Mutex<PoolInner<T>>>);
+pub struct ArcPool<T: PoolEntry>(Arc<Mutex<PoolInner<T>>>);
 
-impl<T> ArcPool<T> {
+impl<T: PoolEntry> ArcPool<T> {
     /// Creates a new `ArcPool` for objects of type T.
     #[inline]
     #[must_use]
@@ -20,57 +21,52 @@ impl<T> ArcPool<T> {
     }
 }
 
-impl<T> Clone for ArcPool<T> {
+impl<T: PoolEntry> Clone for ArcPool<T> {
     fn clone(&self) -> Self {
         Self(self.0.clone())
     }
 }
 
-impl<T> Default for ArcPool<T> {
+impl<T: PoolEntry> Default for ArcPool<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> PrivPoolApi<T> for ArcPool<T> {
+impl<T: PoolEntry> PrivPoolApi for ArcPool<T> {
+    type Entry = T;
+
     #[cfg(not(feature = "parking_lot"))]
     #[inline]
-    fn with_lock<R, F: FnOnce(&mut PoolInner<T>) -> R>(&self, f: F) -> R {
+    fn with_lock<R, F: FnOnce(&mut PoolInner<Self::Entry>) -> R>(&self, f: F) -> R {
         f(&mut self.0.lock().expect("Poisoned Mutex"))
     }
 
     #[cfg(feature = "parking_lot")]
     #[inline]
-    fn with_lock<R, F: FnOnce(&mut PoolInner<T>) -> R>(&self, f: F) -> R {
+    fn with_lock<R, F: FnOnce(&mut PoolInner<Self::Entry>) -> R>(&self, f: F) -> R {
         f(&mut self.0.lock())
     }
 }
 
-impl<T> PoolApi<T> for ArcPool<T> {}
+impl<T: PoolEntry> PoolApi for ArcPool<T> {}
 
-impl<T> SharedPoolApi<T> for ArcPool<T> {}
+impl<T: PoolEntry> SharedPoolApi for ArcPool<T> {}
 
-impl<T> AsSharedPool<T, ArcPool<T>> for ArcPool<T> {
+unsafe impl<T: PoolEntry> ErasablePtr for ArcPool<T> {
+    fn erase(this: Self) -> ErasedPtr {
+        ErasablePtr::erase(this.0)
+    }
+
+    unsafe fn unerase(this: ErasedPtr) -> Self {
+        Self(ErasablePtr::unerase(this))
+    }
+}
+
+impl<T: PoolEntry> CloneSharedPool for ArcPool<T> {
+    type Pool = Self;
     #[inline]
-    fn as_shared_pool(&self) -> &Self {
-        self
+    fn clone_shared_pool(&self) -> Self {
+        self.clone()
     }
-}
-
-#[cfg(test)]
-mod pool_tests {
-    use crate::*;
-
-    #[test]
-    fn smoke() {
-        let _pool: ArcPool<String> = ArcPool::new();
-    }
-}
-
-#[test]
-fn size() {
-    assert_eq!(
-        std::mem::size_of::<UnsafeBox<usize>>(),
-        std::mem::size_of::<[usize; 1]>()
-    );
 }
--- tests/arcpool_box.rs
+++ tests/arcpool_box.rs
@@ -2,48 +2,50 @@ use onsen::*;
 
 #[test]
 fn smoke() {
-    let pool: ArcPool<&str> = ArcPool::new();
+    let pool = ArcPool::new();
     let _mybox = Box::new("Boxed", &pool);
 }
 
 #[test]
-fn size() {
+fn box_is_thin_pointer() {
     assert_eq!(
-        std::mem::size_of::<Box<usize, ArcPool<usize>>>(),
-        std::mem::size_of::<[usize; 2]>()
+        std::mem::size_of::<Box<usize, ArcPool<FatPoolEntry<usize>>>>(),
+        std::mem::size_of::<usize>()
     );
 }
 
 #[test]
 fn frombox() {
-    let pool: ArcPool<&str> = ArcPool::new();
-    let mybox = Box::new("Boxed", &pool);
-    let _my_secondbox = Box::new("Boxed", &mybox);
+    let pool = ArcPool::new();
+    let mybox = Box::new("Boxed1", &pool);
+    let secondbox = Box::new("Boxed2", &mybox);
+    assert_ne!(mybox, secondbox);
 }
 
 #[test]
+#[cfg_attr(miri, ignore)] // miri would report leaked memory here
 fn forget() {
-    let pool: ArcPool<&str> = ArcPool::new();
+    let pool = ArcPool::new();
     Box::forget(Box::new("Boxed", &pool));
 }
 
 #[test]
 fn into_inner() {
-    let pool: ArcPool<&str> = ArcPool::new();
+    let pool = ArcPool::new();
     let v: &str = Box::into_inner(Box::new("Was Boxed", &pool));
     assert_eq!(v, "Was Boxed");
 }
 
 #[test]
 fn deref() {
-    let pool: ArcPool<&str> = ArcPool::new();
+    let pool = ArcPool::new();
     let mybox = Box::new("Boxed", &pool);
     assert_eq!(*mybox, "Boxed");
 }
 
 #[test]
 fn deref_mut() {
-    let pool: ArcPool<&str> = ArcPool::new();
+    let pool = ArcPool::new();
     let mut mybox = Box::new("Boxed", &pool);
     *mybox = "Changed";
     assert_eq!(*mybox, "Changed");
@@ -51,7 +53,7 @@ fn deref_mut() {
 
 #[test]
 fn eq() {
-    let pool: ArcPool<&str> = ArcPool::new();
+    let pool = ArcPool::new();
     let box1 = Box::new("Boxed", &pool);
     let box2 = Box::new("Boxed", &pool);
     let box3 = Box::new("Boxed again", &pool);
